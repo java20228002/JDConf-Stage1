4cca3da175fa8aabefb0ff78b4254d0361086caf
java20228002 <152757775+java20228002@users.noreply.github.com>
2024-01-28 17:32:09 -0600

    update

--- js/jdconf.js
+++ js/jdconf.js
@@ -25,79 +25,79 @@ function getTimeZoneInfo() {
 
 function convertTimezoneToAbbreviation(timezone) {
   const timezoneMap = {
-      "America/Chicago": "CT",
-      "America/New_York": "ET",
-      "America/Los_Angeles": "PT",
-      "America/Denver": "MT",
-      "America/Anchorage": "AKT",
-      "Pacific/Honolulu": "HAT",
-      "Europe/London": "GMT",
-      "Europe/Berlin": "CET",
-      "Europe/Lisbon": "WET",
-      "Europe/Bucharest": "EET",
-      // Add other mappings as needed
+    "America/Chicago": "CT",
+    "America/New_York": "ET",
+    "America/Los_Angeles": "PT",
+    "America/Denver": "MT",
+    "America/Anchorage": "AKT",
+    "Pacific/Honolulu": "HAT",
+    "Europe/London": "GMT",
+    "Europe/Berlin": "CET",
+    "Europe/Lisbon": "WET",
+    "Europe/Bucharest": "EET",
+    // Add other mappings as needed
   };
 
   return timezoneMap[timezone] || timezone;
 }
 
 function convertToUserDate() {
-    // Get all elements with class 'session-time'
-    var sessiondate = document.getElementsByClassName('session-date');
+  // Get all elements with class 'session-time'
+  var sessiondate = document.getElementsByClassName('session-date');
 
-    for (var i = 0; i < sessiondate.length; i++) {
-        // Assume the time is in "HH:MM AM/PM" format and the date is in "DD-MMM-YY" format
-        var dateTime = sessiondate[i].innerText.split(' ');
-        var date = dateTime[0];
-        var stime = dateTime[1] + ' ' + dateTime[3];
+  for (var i = 0; i < sessiondate.length; i++) {
+    // Assume the time is in "HH:MM AM/PM" format and the date is in "DD-MMM-YY" format
+    var dateTime = sessiondate[i].innerText.split(' ');
+    var date = dateTime[0];
+    var stime = dateTime[1] + ' ' + dateTime[3];
 
-        // Convert to a Date object using PST timezone
-        var pstDate = luxon.DateTime.fromFormat(date + ' ' + stime, 'dd-MMM-yy hh:mm a', { zone: 'America/Los_Angeles' });
-        localDate = pstDate.toLocal();
-        localDateTimeString1 = localDate.toFormat ('MMM dd');
-        //localDateTimeString1 = localDate.toLocaleString(luxon.DateTime.DATE_SIMPLE);
-        
-                   
-        // Update the text with the user's local time
-        sessiondate[i].innerText = localDateTimeString1;
-    }
-    }
+    // Convert to a Date object using PST timezone
+    var pstDate = luxon.DateTime.fromFormat(date + ' ' + stime, 'dd-MMM-yy hh:mm a', { zone: 'America/Los_Angeles' });
+    localDate = pstDate.toLocal();
+    localDateTimeString1 = localDate.toFormat('MMM dd');
+    //localDateTimeString1 = localDate.toLocaleString(luxon.DateTime.DATE_SIMPLE);
 
-    function convertToUserTimeZone() {
- 
-    // Get all elements with class 'session-time'
-    var times = document.getElementsByClassName('session-time');
 
-    for (var i = 0; i < times.length; i++) {
-        // Assume the time is in "HH:MM AM/PM" format and the date is in "DD-MMM-YY" format
-        var dateTime = times[i].innerText.split(' ');
-        var date = dateTime[0];
-        var stime = dateTime[1] + ' ' + dateTime[3];
-        var etime = dateTime[2] + ' ' + dateTime[3];
-
-        // Convert to a Date object using PST timezone
-        var pstDate = luxon.DateTime.fromFormat(date + ' ' + stime, 'dd-MMM-yy hh:mm a', { zone: 'America/Los_Angeles' });
-        localDate = pstDate.toLocal();
-        localStartDate = pstDate.toLocal();
-        localDateTimeString1 = localDate.toLocaleString(luxon.DateTime.TIME_SIMPLE);
-
-        // Convert to a Date object using PST timezone
-        var pstDate = luxon.DateTime.fromFormat(date + ' ' + etime, 'dd-MMM-yy hh:mm a', { zone: 'America/Los_Angeles' });
-        localDate = pstDate.toLocal();
-        localDateTimeString2 = localDate.toLocaleString(luxon.DateTime.TIME_SIMPLE);
-
-        timeZone = localDate.offsetNameShort;
-        localDateTimeString3 = localStartDate.toFormat ('MMM dd');
-
-        // Update the text with the user's local time
-        times[i].innerText = localDateTimeString3 + ", " + localDateTimeString1;
-    }
+    // Update the text with the user's local time
+    sessiondate[i].innerText = localDateTimeString1;
   }
-    // Call the functions when the page loads
-    window.onload = function () {
-        convertToUserTimeZone(); // Call the first function
-        convertToUserDate(); // Call the second function
-    };
+}
+
+function convertToUserTimeZone() {
+
+  // Get all elements with class 'session-time'
+  var times = document.getElementsByClassName('session-time');
+
+  for (var i = 0; i < times.length; i++) {
+    // Assume the time is in "HH:MM AM/PM" format and the date is in "DD-MMM-YY" format
+    var dateTime = times[i].innerText.split(' ');
+    var date = dateTime[0];
+    var stime = dateTime[1] + ' ' + dateTime[3];
+    var etime = dateTime[2] + ' ' + dateTime[3];
+
+    // Convert to a Date object using PST timezone
+    var pstDate = luxon.DateTime.fromFormat(date + ' ' + stime, 'dd-MMM-yy hh:mm a', { zone: 'America/Los_Angeles' });
+    localDate = pstDate.toLocal();
+    localStartDate = pstDate.toLocal();
+    localDateTimeString1 = localDate.toLocaleString(luxon.DateTime.TIME_SIMPLE);
+
+    // Convert to a Date object using PST timezone
+    var pstDate = luxon.DateTime.fromFormat(date + ' ' + etime, 'dd-MMM-yy hh:mm a', { zone: 'America/Los_Angeles' });
+    localDate = pstDate.toLocal();
+    localDateTimeString2 = localDate.toLocaleString(luxon.DateTime.TIME_SIMPLE);
+
+    timeZone = localDate.offsetNameShort;
+    localDateTimeString3 = localStartDate.toFormat('MMM dd');
+
+    // Update the text with the user's local time
+    times[i].innerText = localDateTimeString3 + ", " + localDateTimeString1;
+  }
+}
+// Call the functions when the page loads
+window.onload = function () {
+  convertToUserTimeZone(); // Call the first function
+  convertToUserDate(); // Call the second function
+};
 
 
 
@@ -129,10 +129,10 @@ function toggleMenu() {
 
 
 document.querySelectorAll('.nav-menu li a').forEach(item => {
-    item.addEventListener('touchstart', function() {
-        document.querySelector('.nav-menu li a.active').classList.remove('active');
-        this.classList.add('active');
-    });
+  item.addEventListener('touchstart', function () {
+    document.querySelector('.nav-menu li a.active').classList.remove('active');
+    this.classList.add('active');
+  });
 });
 
 
